d35adf868ffc7cf3640aff90fee49ff454b3258a
Andrea Berardi <andrea.berardi@hotmail.com>
2023-04-16 16:42:29 -0300

    Modificación de la app, actualización de la página de directorio con componente directory y componente category-item.

---
+++ src-copia/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { CartProvider } from "./context/CartContext";
+import { LoginProvider } from "./context/LoginContext";
+import AppRouter from "./routes/AppRouter";
+
+function App() {
+  return (
+    <LoginProvider>
+      <CartProvider>
+        <AppRouter />
+      </CartProvider>
+    </LoginProvider>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { CartProvider } from "./context/CartContext";
-import { LoginProvider } from "./context/LoginContext";
-import AppRouter from "./routes/AppRouter";
+import Directory from "./components/directory/directory";
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "deportivos",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "gorras & sombreros",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 4,
+      title: "chicas",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "chicos",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
   return (
-    <LoginProvider>
-      <CartProvider>
-        <AppRouter />
-      </CartProvider>
-    </LoginProvider>
-  );
+    <Directory categories={categories} />
+  )
 }
 
 export default App;
