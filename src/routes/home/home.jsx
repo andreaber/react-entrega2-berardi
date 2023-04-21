aa19d556ab7b5467450dbec4bef793afbbf583d3
Andrea Berardi <andrea.berardi@hotmail.com>
2023-04-21 08:43:24 -0300

    Últimos retoques

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -3,37 +3,9 @@ import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "deportivos",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "gorras & sombreros",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 4,
-      title: "chicas",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "chicos",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <div>
-      <Directory categories={categories} />
+      <Directory />
       <Outlet />
     </div>
   );
